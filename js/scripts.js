22b2d02b81f866fb1bfa86a3e263527931bbb473
jocelyn alsdorf & jeanette fairless <student@epicodus.com>
2015-06-03 13:00:25 -0700

    adds jQuery funcitonality to track turns and mark space with x or o

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@ function Space(coordinate) {
 }
 
 
+
 Space.prototype.markBy = function(player) {
   this.markedby = player;
   player.playedSquare.push(this.coordinate);
@@ -17,11 +18,33 @@ Space.prototype.markBy = function(player) {
 
 
 $(document).ready(function(){
+  var player1 = {};
+  var player2 = {};
+  var turn = 1;
+  $(".start-game").click(function(event){
+    var player1 = new Player("x");
+    var player2 = new Player("y");
+  console.log(player1);
+  console.log(player2);
+});
+
   $(".space").click(function(event){
     event.preventDefault();
-    //$("table").find("td").
-    alert($(this).attr("id") );
+    turn += 1;
 
+    var newSpace = new Space($(this).attr("id"));
+    console.log(newSpace);
+
+    if(turn % 2 === 0) {
+      $(this).html("<span class='inner'>x</span>");
+      newSpace.markedby = "x";
+      console.log(newSpace.markedby);
+    }
+    else {
+      $(this).html("<span class='inner'>o</span>");
+      newSpace.markedby = "o";
+      console.log(newSpace.markedby);
+    }
   });
 
 
